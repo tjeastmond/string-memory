096c8324116fce01331f5f5e657753c7665c9372
TJ Eastmond <tj.eastmond@gmail.com>
2021-04-15 18:02:51 -0400

    A little cleanup

--- src/routes/stress.js
+++ src/routes/stress.js
@@ -3,11 +3,8 @@
  */
 
 module.exports = async function stress(ctx) {
-  const str1 = `Bob's Burgers`;
-  const key1 = await ctx.store.insertOrUpdate(str1);
-  const get1 = await ctx.store.get(key1);
-  const str2 = `Sticking feathers up your butt does not make you a chicken.`;
-  const key2 = await ctx.store.insertOrUpdate(str2);
-  const get2 = await ctx.store.get(key2);
-  ctx.body = { keys: [get1, get2] };
+  const str = `Bob's Burgers`;
+  await ctx.store.insertOrUpdate(str);
+  await ctx.store.get(str);
+  ctx.body = { status: 'ok' };
 };
